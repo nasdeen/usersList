c7a3e9e41f4b43b9c460b5b91cb2b9fffeabe606
2known <61994491+nasdeen@users.noreply.github.com>
2021-05-29 11:35:14 +0000

    updated with Provider store

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { thunk } from "redux-thunk";
+import thunk from "redux-thunk";
 import { createStore, applyMiddleware } from "redux";
 import usersReducer from "./Store/usersReducer";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
 
-const store = createStore(usersReducer, applyMiddleware);
+const store = createStore(usersReducer, applyMiddleware(thunk));
 // const createDispatch = store.dispatch({ type: "SET USERNAME" });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
